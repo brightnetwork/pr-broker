29614530e7978333348d083f64dfb7391fe4509a
guillaume <guillaume@p9f.fr>
2021-05-13 10:41:48 +0100

    chore: first commit

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,30 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+import {updateOutToDatePR} from "./cd";
+import {PRPayload} from "./types";
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const payload = github.context.payload;
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // eslint-disable-next-line no-console
+    console.log(JSON.stringify(payload, null, 2));
 
-    core.setOutput('time', new Date().toTimeString())
+    // payload.repository = {
+    //   name: "pr-broker",
+    //   owner: {
+    //     login: "brightnetwork",
+    //   },
+    // };
+    if (!payload.repository) throw new Error("invalid payload");
+    await updateOutToDatePR({
+      payload: payload as PRPayload,
+      octokit: github.getOctokit(process.env.GITHUB_TOKEN as string),
+    });
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
-run()
+run();
